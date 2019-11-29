0f3dbe1cc4a9eeb8b1be952b7ab95e847cc8f8d3
Nikko Weber <nikkoweber@Nikkos-MacBook-Pro.local>
2019-11-29 08:18:07 -0600

    Removed overlay 2 from JS

--- scripts.js
+++ scripts.js
@@ -6,15 +6,6 @@ function openOverlay1() {
         <iframe src="lube-tech-v2.html"></iframe> 
     `
 }
-
-function openOverlay2() {
-    document.getElementById("overlay").style.display = "flex";
-    document.getElementById("overlay").innerHTML = 
-    `
-        <button onclick="closeOverlay()">Close</button>
-        <iframe src="college-design.html"></iframe> 
-    `
-}
   
 function closeOverlay() {
     document.getElementById("overlay").style.display = "none";
