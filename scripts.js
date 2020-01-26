5de0ff22ae6cabbb166eb6ad472db3199fdd723b
Nikko Weber <nikkoweber@Nikkos-MacBook-Pro.local>
2020-01-26 10:22:09 -0600

    Added bar cribbage

--- scripts.js
+++ scripts.js
@@ -6,6 +6,15 @@ function openOverlay1() {
         <iframe src="lubetech-v2.html"></iframe> 
     `
 }
+
+function openOverlay2() {
+    document.getElementById("overlay").style.display = "flex";
+    document.getElementById("overlay").innerHTML = 
+    `
+        <button onclick="closeOverlay()">Close</button>
+        <iframe src="bar-cribbage.html"></iframe> 
+    `
+}
   
 function closeOverlay() {
     document.getElementById("overlay").style.display = "none";
